c239003831938a04b60d38ede0cde8f6448ce9a3
Tobias Dybdahl <33865716+tdybda17@users.noreply.github.com>
2019-02-12 11:19:32 +0100

    Update and rename program.cs to Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace P4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hejsa");
+        }
+    }
+}
