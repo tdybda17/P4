e2f5433164d3a0fbf872fb8fa54c9754436ba179
EsbenNedergaard <eneder17@student.aau.dk>
2019-03-28 10:43:13 +0100

    Updating attributes

---
+++ src/Compiler/SymbolTable/Table/Symbol/TypeDescriptor/ClassTypeDescriptor/Graph/VertexTypeDescriptor.java
@@ -0,0 +1,10 @@
+package Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.Graph;
+
+import Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.ClassTypeDescriptor;
+
+public class VertexTypeDescriptor  {
+    public VertexTypeDescriptor() {
+        super();
+
+    }
+}
