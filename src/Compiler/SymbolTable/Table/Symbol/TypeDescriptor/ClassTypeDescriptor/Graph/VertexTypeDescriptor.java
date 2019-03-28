3375af93c970222691356302eb8be316963a91ce
EsbenNedergaard <eneder17@student.aau.dk>
2019-03-28 11:13:36 +0100

    Updating attributes

--- src/Compiler/SymbolTable/Table/Symbol/TypeDescriptor/ClassTypeDescriptor/Graph/VertexTypeDescriptor.java
+++ src/Compiler/SymbolTable/Table/Symbol/TypeDescriptor/ClassTypeDescriptor/Graph/VertexTypeDescriptor.java
@@ -1,10 +1,41 @@
 package Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.Graph;
 
 import Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.ClassTypeDescriptor;
+import Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.Field;
+import Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ClassTypeDescriptor.Method;
+import Compiler.SymbolTable.Table.Symbol.TypeDescriptor.ColorTypeDescriptor;
 
-public class VertexTypeDescriptor  {
+public class VertexTypeDescriptor extends ClassTypeDescriptor {
     public VertexTypeDescriptor() {
         super();
-
+        this.addMethods();
+        this.addFields();
     }
+
+    @Override
+    public String getTypeName() {
+        return "Stack";
+    }
+
+    private void addMethods(){
+        this.addMethod(getOutgoingEdges());
+        this.addMethod(getNeighbours());
+    }
+
+    private void addFields() {
+        this.addField(color());
+    }
+
+    private Method getOutgoingEdges(){
+        return null;
+    }
+
+    private Method getNeighbours(){
+        return null;
+    }
+
+    private Field color(){
+        return new Field("color", new ColorTypeDescriptor());
+    }
+
 }
---
+++ src/Compiler/SymbolTable/Table/Symbol/TypeDescriptor/ColorTypeDescriptor.java
@@ -0,0 +1,8 @@
+package Compiler.SymbolTable.Table.Symbol.TypeDescriptor;
+
+public class ColorTypeDescriptor extends SimpleDataTypeDescriptor {
+    @Override
+    public String getTypeName() {
+        return "Integer";
+    }
+}
