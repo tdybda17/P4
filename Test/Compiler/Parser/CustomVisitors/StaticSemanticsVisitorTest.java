15861981eeea55d90cd1eb4861f73c6872413087
EsbenNedergaard <eneder17@student.aau.dk>
2019-04-08 11:14:11 +0200

    Making Static Semantics Visitor

--- Test/Compiler/Parser/CustomVisitors/StaticSemanticsVisitorTest.java
+++ Test/Compiler/Parser/CustomVisitors/StaticSemanticsVisitorTest.java
@@ -1,17 +1,30 @@
 package Compiler.Parser.CustomVisitors;
 
 import Compiler.Parser.GeneratedFiles.TestParser;
+import Compiler.SymbolTable.Table.SymbolTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StaticSemanticsVisitorTest {
+    private SymbolTable symbolTable;
+    private String path;
+
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        path = "Test/Compiler/Parser/CustomVisitors/test";
+        symbolTable = new SymbolTable();
+        symbolTable.openScope();
+        //We use the function visitor to fill up our symbol table with initial values
+        TestParser.useVisitorMethod(new FunctionVisitor(), path, symbolTable);
+    }
+
     @Test
     void visit() throws Exception {
-        String path = "Test/Compiler/Parser/CustomVisitors/test";
-
-        //TestParser.useVisitorMethod(new StaticSemanticsVisitor(), path);
+        TestParser.useVisitorMethod(new StaticSemanticsVisitor(), path, symbolTable);
+        System.out.println(symbolTable);
     }
 
     @Test
