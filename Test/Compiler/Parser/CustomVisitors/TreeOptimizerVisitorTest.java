d2316ce33db82ad00c8606d13d8c01e0517bcd51
EsbenNedergaard <eneder17@student.aau.dk>
2019-05-03 11:42:50 +0200

    Making tree optimizer

--- Test/Compiler/Parser/CustomVisitors/TreeOptimizerVisitorTest.java
+++ Test/Compiler/Parser/CustomVisitors/TreeOptimizerVisitorTest.java
@@ -5,6 +5,7 @@ import Compiler.Parser.GeneratedFiles.TestParser;
 import Compiler.Parser.TreePrinter;
 import Compiler.SymbolTable.Table.SymbolTable;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,22 +22,25 @@ class TreeOptimizerVisitorTest {
         treeOptimizerVisitor = new TreeOptimizerVisitor();
     }
 
+    @Disabled
     @Test
     void visit1() throws Exception{
-        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/Test2";
+        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/TreeOptimizerTest";
         System.out.println(TestParser.parseTextFile(path));
     }
 
+    @Disabled
     @Test
     void visit2() throws Exception {
-        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/Test2";
+        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/TreeOptimizerTest";
         Node root = TestParser.useVisitorMethod(treeOptimizerVisitor, path, childNumberIndex);
         System.out.println(TreePrinter.createDotOutput(root));
     }
 
+    @Disabled
     @Test
     void visit3() throws Exception {
-        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/Test2";
+        String path = "Test/Compiler/Parser/CustomVisitors/TestFiles/TreeOptimizerTest";
         System.out.println("Before:");
         Node root = TestParser.useVisitorMethod(new PrintVisitor(), path, null);
 
