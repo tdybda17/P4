514ed69f4f0383d4979e5024de4af2219313566b
EsbenNedergaard <eneder17@student.aau.dk>
2019-05-03 12:34:21 +0200

    Making a combined visitor

---
+++ src/Compiler/Parser/CustomVisitors/CombinedVisitor.java
@@ -0,0 +1,19 @@
+package Compiler.Parser.CustomVisitors;
+
+import Compiler.Parser.GeneratedFiles.Node;
+import Compiler.SymbolTable.Table.SymbolTable;
+
+public class CombinedVisitor {
+    public static Node visit(Node root) {
+        TreeOptimizerVisitor treeOptimizerVisitor = new TreeOptimizerVisitor();
+        root.jjtAccept(treeOptimizerVisitor, 0);
+
+        SymbolTable symbolTable = new SymbolTable();
+        FunctionVisitor functionVisitor = new FunctionVisitor();
+        root.jjtAccept(functionVisitor, symbolTable);
+
+        StaticSemanticsVisitor staticSemanticsVisitor = new StaticSemanticsVisitor(symbolTable);
+        root.jjtAccept(staticSemanticsVisitor, symbolTable);
+        return root;
+    }
+}
