d6b1a0b50c2067eb8c98871f04c0e646995443a9
EsbenNedergaard <eneder17@student.aau.dk>
2019-05-15 13:35:24 +0200

    made a visitor interface

--- Test/Compiler/Parser/CustomVisitors/StaticSemanticsVisitorTest.java
+++ Test/Compiler/Parser/CustomVisitors/StaticSemanticsVisitorTest.java
@@ -167,10 +167,9 @@ class StaticSemanticsVisitorTest {
         String path = "Test/Compiler/Parser/CustomVisitors/test";
         //We use the function visitor to fill up our symbol table with functions
         Node root = TestParser.createParseTree(path);
-        root.jjtAccept(staticSemanticsVisitor, symbolTable);
         root.jjtAccept(new TreeOptimizerVisitor(), symbolTable);
         root.jjtAccept(new FunctionVisitor(), symbolTable);
-
+        root.jjtAccept(staticSemanticsVisitor, symbolTable);
     }
 
     @Test
